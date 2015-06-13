cea817f4bc3214cf7eea631004177ba4fd6b8e47
Amogh <amogh.91@gmail.com>
2015-06-13 05:40:16 +0000

    pca ready data

--- R/pca_processing.R
+++ R/pca_processing.R
@@ -1,8 +1,15 @@
 #Additional processing for PCA
-notNanList = which(!is.nan(finalFeatureMatrixForTrainingData[,2]))
+
+saveMatrix = finalFeatureMatrixForTrainingData;
+ratingMatrix = ratingVectorForTrainingSet;
+for(i in 1:4){
+  notNanList = which(!is.nan(saveMatrix[,i]))  
+  saveMatrix = as.matrix(saveMatrix[notNanList,])
+  ratingMatrix = as.matrix(ratingMatrix[notNanList])
+}
 
 filename = paste(filepath,"featureMatrixForTrainingData.txt",sep="")
-write.csv(as.matrix(finalFeatureMatrixForTrainingData[notNanList]),filename,row.names = FALSE,col.names=FALSE)
+write.csv(saveMatrix,filename,row.names = FALSE,col.names=FALSE)
 
 filename = paste(filepath,"ratingForTrainingData.txt",sep="")
-write.csv(as.matrix(ratingVectorForTrainingSet[notNanList]),filename,row.names = FALSE,col.names=FALSE)
+write.csv(as.matrix(ratingMatrix),filename,row.names = FALSE,col.names=FALSE)
